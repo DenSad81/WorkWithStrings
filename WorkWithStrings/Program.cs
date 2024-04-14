d35c296df6255a745603ebbefce38d0c63e7df0e
DenSad81 <tagil81@mail.ru>
2024-04-14 09:55:08 +0300

    Добавьте файлы проекта.

---
+++ WorkWithStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        string name;
+        int age;
+        string zodiakSign;
+        int weight;
+
+        Console.Write("Вас зовут: ");
+        name = Console.ReadLine();
+
+        Console.Write("Ваш возраст: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ваш знак зодиака: ");
+        zodiakSign = Console.ReadLine();
+
+        Console.Write("Ваш вес: ");
+        weight = Convert.ToInt32(Console.ReadLine());
+        Console.Write('\n');
+
+        Console.WriteLine($"Вас зовут {name}, ваш возраст {age}, ваш знак зодиака {zodiakSign}, ваш вес {weight}");
+    }
+}
